8b0baca18fe84fc329307bd577b44f4e5d9ce83a
anshulmb <anshultiwarimb@gmail.com>
2025-11-22 11:39:37 +0530

    commit5

--- test4/ec2.tf
+++ test4/ec2.tf
@@ -1,5 +1,5 @@
 module "ec2-instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "6.1.4"
-  subnet_id = "subnet-0cfd39ec8ad1a35bf"
+  subnet_id = "subnet-0cfd3l0c8ad1k92bf"
 }
